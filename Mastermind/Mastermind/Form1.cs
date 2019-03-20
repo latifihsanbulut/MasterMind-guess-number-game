03d06d6e75ac8f7ece9d9b54b041b0df961b2bbc
latifihsanbulut <b21692448@cs.hacettepe.edu.tr>
2019-03-21 01:24:21 +0300

    Mastermind game

---
+++ Mastermind/Mastermind/Form1.cs
@@ -0,0 +1,435 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mastermind
+{
+   
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random(); //Rastgele sayı üretmek için kullanılacak sınıf
+
+        int artı;
+        int eksi;
+        int artı_tutulan;
+        int eksi_tutulan;
+        int yeniDeğer;
+        int eskiDeğer;
+        int yasak_count;                // değişkenler
+        int counter;
+        int random;
+        int array_to_int;
+        int Genel;
+        int tahminin;
+        int sayaç = -1;
+        bool random_flag = false;
+        bool yasak_flag = false;
+
+        int[] tahmin = new int[4];
+        int[] yasak = new int[10];
+        int[] önceki_tahminler = new int[1000];         // Kullanılan Arrayler
+        int[] last_list = new int[4];
+        int[] Tutulan = new int[4];
+        int[] tahmin_edilen = new int[4];
+
+
+
+
+        public void SayiTut() //Basamakları birbirinden farklı 4 basamaklı random sayı üretimi
+        {
+
+            int yeni_sayi; //Her seferinde üretilecek sayımız.
+            bool durum = true;
+
+            for (int i = 0; i < Tutulan.Length; i++)
+            {
+                while (durum)
+                {
+                    yeni_sayi = rnd.Next(0, 10);
+
+                    if (i == 0)
+                    {
+                        Tutulan[0] = yeni_sayi;
+                        break; //While döngüsünden çıkılır.
+                    }
+
+                    //tutulan içersinde oluşturulan yeni sayıdan varmı diye kontrol ediliyor.
+                    //Varsa durum true oluyor ve for döngüsünden çıkıyor. Çünkü yeni bir sayı atamamız gerekiyor.
+                    for (int k = 0; k < i; k++)
+                    {
+                        if (Tutulan[k] == yeni_sayi) //Yeni oluşan sayımız dizide daha önceden varsa
+                        {
+                            durum = true;
+                            break; //for döngüsünden çık
+                        }
+                        else
+                            durum = false;
+                    }
+
+                    if (durum == false)
+                        Tutulan[i] = yeni_sayi;
+                }
+                durum = true;
+
+            }
+
+        }
+
+        public void uret()//Basamakları birbirinden farklı 4 basamaklı random sayı üretimi
+        {
+            
+            int yeni_sayi; //Her seferinde üretilecek sayımız.
+            bool durum = true;
+
+            for (int i = 0; i < tahmin.Length; i++)
+            {
+                while (durum)
+                {
+                    yeni_sayi = rnd.Next(0, 10);
+
+                    if (i == 0)
+                    {
+                        tahmin[0] = yeni_sayi;
+                        break; //While döngüsünden çıkılır.
+                    }
+
+                    //tahmin içersinde oluşturulan yeni sayıdan varmı diye kontrol ediliyor.
+                    //Varsa durum true oluyor ve for döngüsünden çıkıyor. Çünkü yeni bir sayı atamamız gerekiyor.
+                    for (int k = 0; k < i; k++)
+                    {
+                        if (tahmin[k] == yeni_sayi) //Yeni oluşan sayımız dizide daha önceden varsa
+                        {
+                            durum = true;
+                            break; //for döngüsünden çık
+                        }
+                        else
+                            durum = false;
+                    }
+
+                    if (durum == false)
+                        tahmin[i] = yeni_sayi;
+                }
+                durum = true;
+
+            }
+
+            array_to_int = (tahmin[0] * 1000) + (tahmin[1] * 100) + (tahmin[2] * 10) + tahmin[3];
+
+        }
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            groupBox1.Enabled = true;
+
+            uret();
+            textBox1.Text += array_to_int + "\r\n";         // OYUNU BAŞLAT butonuna basıldığında bilgisayarın sayı tutma ve ilk
+            önceki_tahminler[Genel] = array_to_int;         // tahmini yapması
+            Genel++;
+            SayiTut();
+
+            textBox8.Text = "0";
+            textBox9.Text = "0";
+
+            button1.Enabled = false;                        // oyun penceresinin aktif hale getirilmesi 
+            groupBox2.Enabled = true;
+            groupBox1.Enabled = true;
+            button3.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            artı = Convert.ToInt32(textBox8.Text);
+            eksi = Convert.ToInt32(textBox9.Text);            // Bilgisayarın ilk tahmininin ardından bilgisayara gönderilen ipuçları
+
+            yeniDeğer = artı + eksi;
+
+            if (yeniDeğer > 4)           // 4 basamaklı sayılar üzerine yapılan bir oyun olduğu için
+            {                           // ipuçları toplamı da en fazla 4 olabilir.
+                MessageBox.Show("Girdiğiniz ipucu yanlış. Lütfen doğru ipucu giriniz...");
+            }
+
+            else if(yeniDeğer < 5)
+            {
+                textBox6.Text += artı + "\r\n";
+                textBox7.Text += eksi + "\r\n";
+
+                textBox8.Text = "0";
+                textBox9.Text = "0";
+            }
+            
+
+
+            if(yeniDeğer == 0)         // ipuçlarının toplamının "0" olması durumu
+            {
+                for(int i=0; i<tahmin.Length; i++)
+                {
+                    for(int k=0; k<yasak.Length; k++)       // bu durumda bilgisayarın tahmin ettiği 4 basamaklı sayıdaki 
+                    {                                       // hiçbir rakam kullanıcının tuttuğu sayıda yer almadığı için
+                        if(tahmin[i] == yasak[k])           // bu rakamları kullanılmayaklar arrayine ayırmaktadır.
+                        {
+                            yasak_flag = true;
+                            break;
+                        }
+
+                    }
+
+                    if(yasak_flag == false)
+                    {
+                        yasak[yasak_count] = tahmin[i];
+                        yasak_count++;
+
+                    }
+                }
+                Aynı_0:
+                uret();
+                for(int i=0; i<4; i++)
+                {                                               // üretilen sayının daha önce üretilen sayılarla 
+                    for(int k=0; k<yasak.Length; k++)           // aynı olmaması için kıyaslama yapılması
+                    {
+                        if(tahmin[i] == yasak[k])
+                        {
+                            goto Aynı_0;
+                        }
+                    }
+                }
+                for(int i=0; i<önceki_tahminler.Length; i++)
+                {
+                    if (array_to_int == önceki_tahminler[i])
+                    {
+                        goto Aynı_0;
+                    }
+                }
+                önceki_tahminler[Genel] = array_to_int;
+            }
+
+            else if (yeniDeğer == 4)                        // ipucu olarak 4 gönderilmesi durumunda tüm rakamlar
+            {                                               // doğru olduğu için rakamlar arasında yer değişikliği yapılması
+                if(artı == 4)
+                {
+                    MessageBox.Show("BEN KAZANDIM...");
+                    groupBox1.Enabled = false;
+                    groupBox2.Enabled = false;
+                    button1.Enabled = false;
+                }
+
+                ilk_1:
+
+                for (int i = 0; i < 4; i++)
+                {
+                    ilk_2:
+                    random = rnd.Next(0, 4);                    // rakamların rastgele karıştırılması
+
+                    if (tahmin[random] == -1)
+                    {
+                        goto ilk_2;
+                    }
+
+                    last_list[i] = tahmin[random];
+                    tahmin[random] = -1;
+                }
+                                            
+                for(int k=0; k<4; k++)                              
+                {                                                   
+                    tahmin[k] = last_list[k];
+                }
+
+                array_to_int = (tahmin[0] * 1000) + (tahmin[1] * 100) + (tahmin[2] * 10) + tahmin[3];
+
+                
+
+                for (int i = 0; i < önceki_tahminler.Length; i++)           // üretilen sayının daha önce üretilen sayılarla 
+                {                                                           // aynı olmaması için kıyaslama yapılması
+                    if (array_to_int == önceki_tahminler[i])
+                    {
+                        goto ilk_1;
+                    }
+                }
+                önceki_tahminler[Genel] = array_to_int;
+
+                
+            }
+
+            else if(yeniDeğer < 4 && yeniDeğer > 0)         // ipucu olarak 1 2 3 rakamlarının verildiği durumlar
+            {
+                if (yeniDeğer < eskiDeğer)                         // değiştirilen rakam dan dolayı ipucu düzeyi düşerse 
+                {                                                  // değiştirilen rakamın kullanıcının tuttuğu sayının bir  
+                    array_to_int = önceki_tahminler[Genel - 2];    // rakamı olduğunun tespiti ve eski hali üzerinden değişiklikler yapılması
+                    tahmin[0] = array_to_int / 1000;
+                    tahmin[1] = (array_to_int / 100) % 10;
+                    tahmin[2] = (array_to_int / 10) % 10;
+                    tahmin[3] = array_to_int % 10;
+                    counter++;
+                }
+
+                else if (counter >= 4)
+                {
+                    counter = 0;
+                }
+
+                Aynı:
+
+                random_flag = false;
+
+                sayaç++;
+
+                if (sayaç >= 10)
+                {
+                    sayaç = 0;
+                }
+
+                for (int i = 0; i < 10; i++)
+                {
+                    if (sayaç == yasak[i])
+                    {                                       // tahminler için basamak değiştirilirken kullanılacak rakamların
+                        random_flag = true;                 // diğer rakamlardan farklı olarak seçilmesi ve kullanıcının tuttuğu sayıda  
+                        break;                              // kullanılmadığı tespit edilen sayıların kullanılmaması
+                    }
+                }
+
+                for (int i = 0; i < 4; i++)
+                {
+                    if (sayaç == tahmin[i])
+                    {
+                        random_flag = true;
+                        break;
+                    }
+                }
+
+                if (random_flag == true)
+                {
+                    goto Aynı;
+                }
+
+                tahmin[counter] = sayaç;
+                array_to_int = (tahmin[0] * 1000) + (tahmin[1] * 100) + (tahmin[2] * 10) + tahmin[3];
+
+                for (int i = 0; i < önceki_tahminler.Length; i++)           // üretilen sayının daha önce üretilen sayılarla 
+                {                                                           // aynı olmaması için kıyaslama yapılması
+                    if (array_to_int == önceki_tahminler[i])
+                    {
+                        goto Aynı;
+                    }
+                }
+                önceki_tahminler[Genel] = array_to_int;
+
+            }
+
+
+            if (yeniDeğer < 4 && yeniDeğer >= 0)
+            {
+                if ((array_to_int / 1000) == 0)
+                {
+                    textBox1.Text += "0" + array_to_int + "\r\n";       // üretilen sayının yazdırılması
+
+                }
+
+                else
+                {
+                    textBox1.Text += array_to_int + "\r\n";
+
+                }
+
+                eskiDeğer = yeniDeğer;
+                Genel++;
+                button2.Enabled = false;                    // sıranın kullanıcıya geçmesi
+                button3.Enabled = true;
+
+            }
+
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+                        
+            if(textBox5.Text.Length < 4 || textBox5.Text.Length > 4)
+            {
+                MessageBox.Show("Lütfen 4 Basamaklı Tahmininizi Girin");    // Hata Kontrolü
+
+            }
+
+            else
+            {
+                tahminin = Convert.ToInt32(textBox5.Text);
+                int a = tahminin / 1000;
+                int b = (tahminin / 100) % 10;
+                int c = (tahminin / 10) % 10;
+                int d = tahminin % 10;
+
+                if (a == b || a == c || a == d || b == c || b == d || c == d)
+                {
+                    MessageBox.Show("Birbirinden farklı rakamlar giriniz.");    // Hata Kontrolü
+
+                }
+
+                else
+                {
+                    textBox5.Text = "";
+                    tahmin_edilen[0] = tahminin / 1000;
+                    tahmin_edilen[1] = (tahminin / 100) % 10;           // Kullanıcıdan alınan 4 basamaklı sayının  
+                    tahmin_edilen[2] = (tahminin / 10) % 10;            // basamaklarına ayrılması
+                    tahmin_edilen[3] = tahminin % 10;
+
+                    for (int i = 0; i < 4; i++)
+                    {
+                        if (Tutulan[i] == tahmin_edilen[i])
+                        {
+                            artı_tutulan++;                   // Basamak değerleri ve rakam değerleri doğru bilinen rakamların tespiti 
+                        }
+                    }
+
+                    for (int i = 0; i < 4; i++)
+                    {
+                        for (int k = 0; k < 4; k++)
+                        {
+                            if (Tutulan[i] == tahmin_edilen[k])
+                            {                                           //rakam değerleri doğru bilinen rakamların tespiti
+                                eksi_tutulan++;
+                            }
+                        }
+                    }
+
+                    eksi_tutulan = eksi_tutulan - artı_tutulan;
+
+                    textBox2.Text += tahminin + "\r\n";                   // kullanıcının tahmin ettiği sayının CPU tarafından değerlendirilip 
+                    textBox3.Text += artı_tutulan + "\r\n";               // ipuclarının Kullanıcıya verilmesi
+                    textBox4.Text += eksi_tutulan + "\r\n";
+
+                    if (artı_tutulan == 4)
+                    {
+                        MessageBox.Show("TEBRİKLER KAZANDINIZ...");
+                        groupBox1.Enabled = false;                         // Kullanıcının doğru tahmine ulaşma durumu
+                        groupBox2.Enabled = false;
+                        button1.Enabled = false;
+                    }
+
+                    artı_tutulan = 0;
+                    eksi_tutulan = 0;
+
+                    button2.Enabled = true;
+                    button3.Enabled = false;
+                }
+            
+            }
+        }
+    }
+}
---
+++ Mastermind/Mastermind/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Mastermind")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mastermind")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("4e5cd2af-8d2f-4dbe-8a62-82bbf288b94d")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// '*' kullanarak varsayılana ayarlayabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
